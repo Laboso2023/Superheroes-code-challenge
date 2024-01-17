6e447b8fb9dbe91cae0d4a4e245f27362f68f42c
mwangiowen <owen31588@gmail.com>
2024-01-17 18:47:13 +0300

    Done

--- python-code-challenge-superheroes/code-challenge/app/seed.py
+++ python-code-challenge-superheroes/code-challenge/app/seed.py
@@ -1,6 +1,6 @@
 from app import app, db
-from models import Hero
-from test import all_names
+from models import Hero, Power, HeroPower
+from test import all_names, powers_data, heros_powers_data
 
 
 def hero_add():
@@ -10,8 +10,28 @@ def hero_add():
         ]
         db.session.bulk_save_objects(hero_objects)
         db.session.commit()
+        
+        
+def power_add():
+    with app.app_context():
+        power_objects = [
+            Power(name=power["name"], description=power["description"]) for power in powers_data
+        ]
+
+        db.session.bulk_save_objects(power_objects)
+        db.session.commit()
+
+def hero_power_add():
+    with app.app_context():
+        hero_power_objects = [
+            HeroPower(hero_id=hero["hero_id"], power_id=hero["power_id"]) for hero in heros_powers_data
+        ]
+
+        db.session.bulk_save_objects(hero_power_objects)
+        db.session.commit()
 
 
-
-
-# hero_add()
+# if __name__ == '__main__':
+    # hero_power_add()
+    # power_add()
+    # hero_add()
